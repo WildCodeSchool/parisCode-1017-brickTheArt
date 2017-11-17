a1ed697a6357df528476d273a69dfb14013611f6
Cyrht <cyr.huet@gmail.com>
2017-11-17 10:04:52 +0100

    create Assets and add Banner

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ if (isset ($_GET['page'])) {
 
         }
 
-    if ($_GET['page'] == 'contact') {
+        if ($_GET['page'] == 'contact') {
         $defaultController = new DefaultController();
         echo $defaultController->contactAction();
 
