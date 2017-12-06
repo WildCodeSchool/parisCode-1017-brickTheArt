14063757f1e16315686a379ec5d28894179ac22c
Cindy <cindy.liwenge@gmail.com>
2017-12-06 17:24:15 +0100

    Add new dab with updated marker table

---
+++ bdd/projet2.sql
@@ -0,0 +1,152 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: projet2
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.17.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contact_information`
+--
+
+DROP TABLE IF EXISTS `contact_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact_information` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` int(10) DEFAULT NULL,
+  `adress` varchar(255) DEFAULT NULL,
+  `opening` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact_information`
+--
+
+LOCK TABLES `contact_information` WRITE;
+/*!40000 ALTER TABLE `contact_information` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact_information` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `informations`
+--
+
+DROP TABLE IF EXISTS `informations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `informations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `home` text,
+  `concept` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `informations`
+--
+
+LOCK TABLES `informations` WRITE;
+/*!40000 ALTER TABLE `informations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `informations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marker`
+--
+
+DROP TABLE IF EXISTS `marker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `event` varchar(45) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marker`
+--
+
+LOCK TABLES `marker` WRITE;
+/*!40000 ALTER TABLE `marker` DISABLE KEYS */;
+INSERT INTO `marker` VALUES (1,55.8642,-4.25181,'BrickTheArt visite Glasgow','Pour la première fois, le Lego Truck se posera en Ecosse !'),(2,45.4408,12.3155,'Enfin à Venise','Et si nous faisions tenir notre construction sur une gondole en circulation ?');
+/*!40000 ALTER TABLE `marker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `masterpiece`
+--
+
+DROP TABLE IF EXISTS `masterpiece`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `masterpiece` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `image` text,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `masterpiece`
+--
+
+LOCK TABLES `masterpiece` WRITE;
+/*!40000 ALTER TABLE `masterpiece` DISABLE KEYS */;
+/*!40000 ALTER TABLE `masterpiece` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(10) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-06 17:21:01
