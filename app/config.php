e14bf58e46afcdb5fbe45a1dc64a4987fed82498
Cindy <cindy.liwenge@gmail.com>
2017-12-08 11:59:54 +0100

    add config bdd for heroku

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('APP_DB_NAME', 'sql11209715');
+define('APP_DB_USER', 'sql11209715');
+define('APP_DB_PWD', 'N7NWkRz3qa');
+define('APP_DB_HOST', 'sql11.freemysqlhosting.net');
+define('APP_DB_PORT', '3306');
+
