2f11f93bd52023f90212d19357539f4a8b43e3d8
kinedie <cindy.liwenge@gmail.com>
2019-09-03 15:28:53 +0200

    Update Twig initialization

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -2,32 +2,22 @@
 
 namespace BrickTheArt\Controllers;
 
-use BrickTheArt\Model\Repository\ContactManager;
-use Twig_Loader_Filesystem;
-use Twig_Environment;
-use Twig_Extension_Debug;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
 
-/**
- * Class DefaultController
- */
 class DefaultController
 {
-	/**
-	 * @var Twig_Environment
-	 */
+	/** @var Environment */
 	protected $twig;
 
-	/**
-	 * Twig DefaultController constructor.
-	 */
 	public function __construct()
 	{
-		$loader = new Twig_Loader_Filesystem('../src/Views/');
-		$this->twig = new Twig_Environment($loader, array(
+		$loader = new FilesystemLoader('../src/Views/');
+		$this->twig = new Environment($loader, array(
 			'cache' => false,
 			'debug' => true
 		));
-		$this->twig->addExtension(new Twig_Extension_Debug());
+		$this->twig->addExtension(new DebugExtension());
 	}
-
 }
